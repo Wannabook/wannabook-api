775f449e9d42531d8989142f145ca0a55685a961
Artur Fralou <artur.fralou@gmail.com>
2019-04-07 12:23:44 +0300

    WNB-62: Add config.js for dev environment, npm save-exact mode

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const test = require('./routes/test');
+const config = require('./config/config');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.json({msg: 'Wannabook!'}));
 
 app.use('/test', test);
-const port = process.env.PORT || 5000;
+
+const devPort = config.app.port;
+const port = process.env.PORT || devPort;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
